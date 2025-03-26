64bb6b550d5ae8f53b8649252195b4eb8a868d23
FJ8ME <fjavierme818@gmail.com>
2025-03-25 19:05:22 -0600

    Feat: Add product endpoints and update services
    - Added core service functions in `products/services.py`:
      - `create_product`: Handles product creation logic.
      - `get_product`: Retrieves a product by ID.
      - `get_all_products`: Fetches all available products.
      - `update_product`: Updates product details.
      - `delete_product`: Removes a product from the database.
    
    - Improved error handling and validations in service layer.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from login.routes import user
+from products.routes import products
 
 app = FastAPI(title="11vo Solutions API", version="0.1.0")
 
 app.include_router(user.router, prefix="/auth", tags=["auth"])
+app.include_router(products.router, prefix="/products", tags=["products"])
 
 @app.get("/")
 def root():
